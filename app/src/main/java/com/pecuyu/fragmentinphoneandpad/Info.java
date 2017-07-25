e1f5bf630ca7fa533c8d801de570b92df5f0ef71
pecuyu <pecuyu@outlook.com>
2017-07-25 23:58:14 +0800

    add some xml and code

---
+++ app/src/main/java/com/pecuyu/fragmentinphoneandpad/Info.java
@@ -0,0 +1,8 @@
+package com.pecuyu.fragmentinphoneandpad;
+
+/**
+ * Created by pecuyu on 2017/7/25.
+ */
+
+public class Info {
+}
---
+++ app/src/main/java/com/pecuyu/fragmentinphoneandpad/MyRecyclerViewAdapter.java
@@ -0,0 +1,9 @@
+package com.pecuyu.fragmentinphoneandpad;
+
+
+/**
+ * Created by pecuyu on 2017/7/25.
+ */
+
+public class MyRecyclerAdapter extends {
+}
